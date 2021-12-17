a737fa5757c13c7d8326e9c3742039894762d628
Jean Whitmore <jeanimal@gmail.com>
2021-12-17 10:24:33 -0600

    Fixed bug where intercept sometimes sampled out.  Better pseudo inverse function GREATLY reduced noise-- a game-changer.

--- src/bias_variance.R
+++ src/bias_variance.R
@@ -1,16 +1,23 @@
 # Functions to illustrate the bias-variance trade-off and double descent.
 
-library(tidyverse)
-library(ggplot2)
+library(psych) # For pseudo-inverse
 
-# Function for computing Moore-Penrose pseudo-inverse via SVD.
-# From Ben at
-# https://stats.stackexchange.com/questions/461423/moore-penrose-pseudo-inverse-fast-algorithm-in-r
-pseudo_inv <- function(matrix) {
-  SVD <- svd(matrix);
-  DDD <- rep(0, length(SVD$d));
-  for (i in 1:length(DDD)) { DDD[i] <- ifelse(SVD$d[i] == 0, 0, 1/SVD$d[i]);  }
-  SVD$v %*% diag(DDD) %*% t(SVD$u); 
+# TODO(jean): Use and test this.
+splitTrainTest <- function(X, y, n_train, p) {
+  train_idx <- sample(nrow(X), size=n_train, replace=FALSE)
+  train_col <- sample((2:ncol(X)), size=p, replace=FALSE)
+  X_train <- X[train_idx, train_col]
+  X_train <- cbind(1, X_train)  # Add intercept.
+  y_train <- y[train_idx]
+  X_test  <- X[-(1:n_train), train_col]
+  X_test <- cbind(1, X_test)  # Add intercept.
+  y_test  <- y[-(1:n_train)]
+  list (
+    X_train = X_train,
+    y_train = y_train,
+    X_test = X_test,
+    y_test = y_test
+  )
 }
 
 # We need a form of linear regression that can handle more parameters than rows-- 
@@ -24,7 +31,9 @@ pseudo_inv <- function(matrix) {
 # Code from https://m-clark.github.io/posts/2021-10-30-double-descent/
 fit_ridgeless = function(X_train, y, X_test, y_test){
   # get the coefficient estimates
-  b <- pseudo_inv(crossprod(X_train)) %*% crossprod(X_train, y)
+  # Note: The pseudo-inverse function can have a lot of rounding error.
+  # If you do not use the one from the psych package, choose wisely.
+  b <- psych::Pinv(crossprod(X_train)) %*% crossprod(X_train, y)
   
   # get training/test predictions
   predictions_train <- X_train %*% b
@@ -36,9 +45,10 @@ fit_ridgeless = function(X_train, y, X_test, y_test){
   
   # return result
   list(
-    b = b,
-    predictions_train = predictions_train,
-    predictions_test  = predictions_test,
+    # Commented out unneeded outputs
+    # b = b,
+    # predictions_train = predictions_train,
+    # predictions_test  = predictions_test,
     rmse_train = rmse_train,
     rmse_test  = rmse_test
   )
